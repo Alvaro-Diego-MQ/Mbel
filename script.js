369ecc1e208a801aab81f1c0dd2d33b7c4a51a10
Alvaro-Diego-MQ <78985936+Alvaro-Diego-MQ@users.noreply.github.com>
2025-02-14 20:46:13 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function startValentine() {
-    document.getElementById("startButton").style.display = "none"; // Oculta el botón
-    document.getElementById("container").style.display = "block"; // Muestra el corazón y mensaje
-
-    let audio = document.getElementById("loveSong");
-    audio.play().catch(error => console.log("Error al reproducir el audio:", error));
-}
